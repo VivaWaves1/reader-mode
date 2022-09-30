11017e8cdf6ba7543e295adb3eab5d258d00e713
Joseph Tessitore <joetgamerman3@gmail.com>
2022-09-30 18:46:41 -0400

    Convert to tabs and organize :)

--- HttpExample/index.ts
+++ HttpExample/index.ts
@@ -3,111 +3,72 @@ import { Readability } from "@mozilla/readability";
 import { JSDOM } from "jsdom";
 import axios from "axios";
 
+import renderArticle from "./template";
+
 // Get Host header from azure function
 function getHost(req: HttpRequest): string {
-    if (req.headers && req.headers["host"]) {
-        return req.headers["host"];
-    }
-    return "";
+	if (req.headers && req.headers["host"]) {
+		return req.headers["host"];
+	}
+	return "";
 }
 
 // normalize url
 function normalizeURL(url: string): string {
-    if (!url.startsWith("http")) {
-        url = "http://" + url;
-    }
-    return url;
+	if (!url.startsWith("http")) {
+		url = "http://" + url;
+	}
+	return url;
 }
 
 function rerouteLinks(html: JSDOM, url: string, base: string, https: boolean): void {
-    const links = html.window.document.querySelectorAll("a");
-    for (const link of links) {
-        // check if link is relative
-        if (link.href.startsWith("/")) {
-            // set link to absolute
-            link.href = url + link.href;
-        }
-        
-        if (https) {
-            link.href = `https://${base}/?q=${link.href}`;
-        } else {
-            link.href = `http://${base}/?q=${link.href}`;
-        }
+	const links = html.window.document.querySelectorAll("a");
+	for (const link of links) {
+		// check if link is relative
+		if (link.href.startsWith("/")) {
+			// set link to absolute
+			link.href = url + link.href;
+		}
+		
+		if (https) {
+			link.href = `https://${base}/?q=${link.href}`;
+		} else {
+			link.href = `http://${base}/?q=${link.href}`;
+		}
 
-    }
+	}
 }
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    let url = req.query.q;
-    if (!url) {
-        context.res = {
-            status: 400,
-            body: "Please pass a url on the query string or in the request body. Example: https://read.jonot.me/?q=https://www.nytimes.com/2020/05/29/us/politics/trump-coronavirus.html"
-        };
-        return;
-    }
-    const response = await axios.get(normalizeURL(url));
-    const body = response.data;
-    const doc = new JSDOM(body);
-    rerouteLinks(doc, normalizeURL(url), getHost(req), req.headers["x-forwarded-proto"] === "https");
-    let reader = new Readability(doc.window.document);
-    let article = reader.parse();
-    if (article === null) {
-        context.res = {
-            status: 400,
-            body: "Could not parse article :("
-        };
-        return
-    }
-    let template = `
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-    body {
-        max-width: 70ch;
-        padding: 3em 1em;
-        margin: auto;
-        line-height: 1.75;
-        font-size: 1.25em;
-    }
-    a {
-        color: #08f;
-    }
-    img {
-        width: 100%;
-        height: auto;
-    }
-    @media (prefers-color-scheme: dark) {
-        body {
-            background-color: #222;
-            color: #eee;
-        }
-        a {
-            color: #9cf;
-        }
-    }
-    </style>
-    <title>${article.title || "Reader Mode"}</title>
-    <base href="${url}"/>
-</head>
-<body>
-    <h1>${article.title || ""}</h1>
-    <h2>${article.byline || ""}</h2>
-    ${article.content}
-</body>
-</html>`
+	let url = req.query.q;
+	if (!url) {
+		context.res = {
+			status: 400,
+			body: "Please pass a url on the query string or in the request body. Example: https://read.jonot.me/?q=https://www.nytimes.com/2020/05/29/us/politics/trump-coronavirus.html"
+		};
+		return;
+	}
+	const response = await axios.get(normalizeURL(url));
+	const body = response.data;
+	const doc = new JSDOM(body);
+	rerouteLinks(doc, normalizeURL(url), getHost(req), req.headers["x-forwarded-proto"] === "https");
+	let reader = new Readability(doc.window.document);
+	let article = reader.parse();
+	if (article === null) {
+		context.res = {
+			status: 400,
+			body: "Could not parse article :("
+		};
+		return
+	}
 
-    let res: HttpResponse = {
-        body: template,
-        headers: {
-            "Content-Type": "text/html"
-        }
-    }
-    context.res = res;
+	let res: HttpResponse = {
+		body: renderArticle(article, url),
+		headers: {
+			"Content-Type": "text/html"
+		}
+	}
+	context.res = res;
 
 };
 
---
+++ HttpExample/template.ts
@@ -0,0 +1,59 @@
+import { Readability } from "@mozilla/readability";
+
+interface Article {
+	title: string;
+	byline: string;
+	dir: string;
+	content: string;
+	textContent: string;
+	length: number;
+	excerpt: string;
+	siteName: string;
+}
+
+// Put article into template
+function renderArticle(article: Article, url: string): string {
+	return `
+		<!DOCTYPE html>
+		<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta http-equiv="X-UA-Compatible" content="IE=edge">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<style>
+					body {
+						max-width: 70ch;
+						padding: 3em 1em;
+						margin: auto;
+						line-height: 1.75;
+						font-size: 1.25em;
+					}
+					a {
+						color: #08f;
+					}
+					img {
+						width: 100%;
+						height: auto;
+					}
+					@media (prefers-color-scheme: dark) {
+						body {
+							background-color: #222;
+							color: #eee;
+						}
+						a {
+							color: #9cf;
+						}
+					}
+				</style>
+				<title>${article.title || "Reader Mode"}</title>
+				<base href="${url}"/>
+			</head>
+			<body>
+				<h1>${article.title || ""}</h1>
+				<h2>${article.byline || ""}</h2>
+				${article.content}
+			</body>
+		</html>`
+}
+
+export default renderArticle;
