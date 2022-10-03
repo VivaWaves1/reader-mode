6d86e2b1b8281b76be4c93e5c90b93d69ac707ec
Tristan F <LeoDog896@hotmail.com>
2022-10-03 10:39:50 -0400

    fmt

--- AboutPage/index.ts
+++ AboutPage/index.ts
@@ -1,5 +1,5 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
-import page from "./index.html"
+import page from "./index.html";
 
 const httpTrigger: AzureFunction = async function (
 	context: Context,
--- build.js
+++ build.js
@@ -1,16 +1,28 @@
-const esbuild = require('esbuild');
+const esbuild = require("esbuild");
 
 /** @type esbuild.BuildOptions */
 const buildOptions = {
-    bundle: true,
-    platform: "node",
-    target: "node16",
-    sourcemap: "linked",
-    loader: {
-        ".html": "text"
-    },
-    external: ["./node_modules/*"]
-}
+	bundle: true,
+	platform: "node",
+	target: "node16",
+	sourcemap: "linked",
+	loader: {
+		".html": "text",
+	},
+	external: ["./node_modules/*"],
+};
 
-esbuild.build({ ...buildOptions, entryPoints: ['AboutPage/index.ts'], outfile: 'dist/AboutPage/index.js' }).catch(() => process.exit(1))
-esbuild.build({ ...buildOptions, entryPoints: ['HttpExample/index.ts'], outfile: 'dist/HttpExample/index.js' }).catch(() => process.exit(1))
+esbuild
+	.build({
+		...buildOptions,
+		entryPoints: ["AboutPage/index.ts"],
+		outfile: "dist/AboutPage/index.js",
+	})
+	.catch(() => process.exit(1));
+esbuild
+	.build({
+		...buildOptions,
+		entryPoints: ["HttpExample/index.ts"],
+		outfile: "dist/HttpExample/index.js",
+	})
+	.catch(() => process.exit(1));
